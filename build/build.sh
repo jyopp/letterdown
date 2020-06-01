1edde7edd137c23668e59c34163575d3936b450e
James Yopp <git@jyopp.com>
2020-06-01 13:37:36 -0700

    Don't recurse when building from the script

--- build/build.sh
+++ build/build.sh
@@ -8,12 +8,12 @@ PRIVATE_KEY=${3}
 
 if [ -n "$PRIVATE_KEY" ]; then
     # Create an UNCOMPRESSED temporary archive to sign
-    tar -cv -f "temp.ltd" --format=ustar -T "$FILES_LIST"
+    tar -cv -f "temp.ltd" --format=ustar -n -T "$FILES_LIST"
     openssl dgst -sha256 -sign "$PRIVATE_KEY" -out "signature" temp.ltd
     # Copy the public key
     openssl rsa -in "$PRIVATE_KEY" -pubout -out pubkey.pem
     # Copy everything in order to the final output archive.
     tar -cvz -f "$FILENAME" @temp.ltd signature pubkey.pem
 else
-    tar -cvz -f "$FILENAME" --format=ustar -T "$FILES_LIST"
+    tar -cvz -f "$FILENAME" --format=ustar -n -T "$FILES_LIST"
 fi
